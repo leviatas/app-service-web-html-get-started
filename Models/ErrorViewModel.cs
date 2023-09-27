55ab04d4332dd012795b7d8751c574d876a3dda0
Eduardo Peluffo <ePeluffo@hospitalaustral.edu.ar>
2023-09-27 11:32:33 -0300

    CHANGE: Change to MVC Proyect

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Leviatas.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
